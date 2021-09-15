d75a991f11c390db99214be7883b02a12706b1a2
Christopher R. Schuchardt <Chris.Schuchardt@pcworldinc.com>
2021-09-15 05:46:58 -0400

    Bug

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="@/assets/logo.png"> <!-- I work -->
+  <img alt="Vue logo" src="@/assets/logo i have a spaces.png"> <!--  i dont work -->
   <h1>Welcome to Your Vue.js App</h1>
 </template>
